fe61a133cb44ba6d9cdbf4ac16411f8af6ec98c1
jimlinJC <jinchglin@gmail.com>
2020-07-03 13:47:18 +0800

    commit_0703_1

---
+++ js/script.js
@@ -0,0 +1,16 @@
+$(function() {
+    //     $(window).scroll(function() {
+
+    //         //use this number to determine when to switch to fixed menu
+    //         console.log($(window).scrollTop())
+
+    //         if ($(window).scrollTop() > 251) $('#navbar-example2').addClass('fixed-top');
+
+    //         if ($(window).scrollTop() < 250) $('#navbar-example2').removeClass('fixed-top');
+
+    //     });
+
+    // $('body').scrollspy({ target: '#navbar-example2' });
+
+
+});
